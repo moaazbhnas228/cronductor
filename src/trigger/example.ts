fb4fab67ce2641c50d056dd3268cb7d02130418f
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-02-13 08:37:17 +0200

    Finished first cron

--- src/trigger/example.ts
+++ src/trigger/example.ts
@@ -1,28 +1,44 @@
 import { logger, schedules, wait } from '@trigger.dev/sdk/v3';
+import { format } from 'date-fns';
+import { getSuccessfulTransactionsFromTo } from '../db/sdk';
 
-export const firstScheduledTask = schedules.task({
-  id: 'second-scheduled-task',
-  // Every hour
-  cron: '0 * * * *',
+export const successfulTransactions = schedules.task({
+  id: 'successful-transactions',
+  // Every 5 mins
+  cron: '*/5 * * * *',
   // Set an optional maxDuration to prevent tasks from running indefinitely
   maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
   run: async (payload, { ctx }) => {
-    // The payload contains the last run timestamp that you can use to check if this is the first run
-    // And calculate the time since the last run
-    const distanceInMs =
-      payload.timestamp.getTime() -
-      (payload.lastTimestamp ?? new Date()).getTime();
+    const to = format(payload.timestamp, 'yyyy-MM-dd HH:mm:ss'); // current timestamp from payload
+    const from = format(payload.lastTimestamp, 'yyyy-MM-dd HH:mm:ss'); // 5 minutes ago
 
-    logger.log('First scheduled tasks', { payload, distanceInMs });
-
-    // Wait for 5 seconds
-    await wait.for({ seconds: 5 });
-
-    // Format the timestamp using the timezone from the payload
-    const formatted = payload.timestamp.toLocaleString('en-US', {
-      timeZone: payload.timezone
+    logger.info(`🔵 Getting successful transactions from ${from} to ${to}`, {
+      from,
+      to
     });
 
-    logger.log(formatted);
+    const transactionsResult = await getSuccessfulTransactionsFromTo(from, to);
+
+    if (transactionsResult.isErr()) {
+      logger.error(`🔴 Error getting successful transactions from ${from} to ${to}`, {
+        error: transactionsResult.error,
+        from,
+        to
+      });
+      throw new Error(transactionsResult.error.message);
+    }
+
+    if (transactionsResult.value.length === 0) {
+      logger.info(`🔴 No successful transactions found from ${from} to ${to}`, {
+        from,
+        to
+      });
+      throw new Error(`🔴 No successful transactions found from ${from} to ${to}`);
+    }
+
+    logger.info(
+      `🟢 Successfully got ${transactionsResult.value.length} successful transactions from ${from} to ${to}`,
+      { transactions: transactionsResult.value }
+    );
   }
 });
